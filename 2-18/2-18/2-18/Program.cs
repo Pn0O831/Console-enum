1ca4ae47714ba74cda4ec353bd98474dca57f7cd
Pn0 <superumesyu@gmail.com>
2025-12-08 15:11:43 +0900

    課題2-18について

---
+++ 2-18/2-18/2-18/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Employee
+{
+    private string EmployeeId;
+    private string Name;
+
+    public Employee(string employeeId, string name)
+    {
+        this.EmployeeId = employeeId;
+        this.Name = name;
+    }
+    public string Employee_Id
+    {
+        get { return EmployeeId; }
+        set { EmployeeId = value; }
+    }
+    public string _Name
+    {
+        get { return Name; }
+        set { Name = value; }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Employee EInfo = new Employee("E002", "田中花子");
+        Console.Write("社員ID:" + EInfo.Employee_Id + "," + "名前:" + EInfo._Name);
+    }
+}
