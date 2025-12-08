60a87a0be9a37c7c3608da3403723811ba866a6b
Pn0 <superumesyu@gmail.com>
2025-12-08 11:14:40 +0900

    課題2-17について

---
+++ 2-17/2-17/2-17/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//①
+Console.WriteLine("Hello, World!");
+
+//②
+string num1 = new string("こんにちは、");
+string num2 = new string("はじめまして");
+Console.WriteLine(num1 + num2);
