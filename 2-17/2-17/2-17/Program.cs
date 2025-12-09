5f7671dcd94501376d123ef58c111317af4c30f8
Pn0 <superumesyu@gmail.com>
2025-12-09 10:40:35 +0900

    ②の記述について修正しました

--- 2-17/2-17/2-17/Program.cs
+++ 2-17/2-17/2-17/Program.cs
@@ -1,9 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-//①
-Console.WriteLine("Hello, World!");
+using System;
 
-//②
-string num1 = new string("こんにちは、");
-string num2 = new string("はじめまして");
-Console.WriteLine(num1 + num2);
+class Program
+{
+    static void Main()
+    {
+        //①
+        Console.WriteLine("Hello, World!");
+
+        //②
+        string stc1 = Console.ReadLine();
+        string stc2 = Console.ReadLine();
+
+        Console.WriteLine($"{stc1}{stc2}");
+    }
+}
--- 2-18/2-18/2-18/Program.cs
+++ 2-18/2-18/2-18/Program.cs
@@ -27,6 +27,87 @@ class Program
     static void Main()
     {
         Employee EInfo = new Employee("E002", "田中花子");
-        Console.Write("社員ID:" + EInfo.Employee_Id + "," + "名前:" + EInfo._Name);
+        Console.WriteLine("社員ID:" + EInfo.Employee_Id + "," + "名前:" + EInfo._Name);
+
+        //Copilot練習問題１
+        Student a = new Student("山田太郎", 101);
+        Console.WriteLine($"{a.Name}のスコア{a.Score}");
+
+        //Copilot練習問題2
+        Worker s = new Worker("")
+    }
+    static void Next()
+    {
+    }
+}
+
+class Student
+{
+    private string name;
+    private int score;
+
+    public Student(string name, int score)
+    {
+        this.Name = name;
+        this.Score = score;
+    }
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    public int Score
+    {
+        get { return score; }
+        set
+        {
+            if (0 <= value && value <= 100)
+            {
+                Score = value;
+            }
+            else
+            {
+                Console.WriteLine("数値は0～100の範囲で入力してください");
+            }
+        }
+    }
+}
+
+class Worker
+{
+    private string id;
+    private string name;
+    private int salary;
+
+    public Worker(string id, string name, int salary)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.Salary = salary;
+    }
+
+    public string Id
+    {
+        get { return id; }
+    }
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    public int Salary
+    {
+        get { return salary; }
+        set
+        {
+            if (value >= 0)
+            {
+                salary = value;
+            }
+            else
+            {
+                Console.WriteLine("エラー:給与は0以上の数字で入力してください");
+            }
+        }
     }
 }
