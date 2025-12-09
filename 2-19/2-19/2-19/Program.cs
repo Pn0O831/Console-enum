e266e1e1f89f2fabc9a95820e07e1d14c7fe8cf5
Pn0 <superumesyu@gmail.com>
2025-12-09 13:38:27 +0900

    課題2-19について

---
+++ 2-19/2-19/2-19/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        TestInfo name = new TestInfo("山田", 20, 170, 60);
+
+    }
+}
+
+class TestPerson
+{
+    public string name;
+    public int age;
+
+    public TestPerson(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+        Console.WriteLine("基底クラス");
+        Console.WriteLine($"名前：{name},年齢：{age}");
+
+    }
+}
+class TestInfo : TestPerson
+{
+    public int height;
+    public int weight;
+    public TestInfo(string name,int age,int height, int weight) : base(name,age)
+    {
+        this.height = height;
+        this.weight = weight;
+        Console.WriteLine("派生クラス");
+        Console.WriteLine($"体重：{height},年齢：{weight}");
+    }
+}
