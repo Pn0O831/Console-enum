2170fdad4613875a83f2d0c20551567b9b1d135b
Pn0 <superumesyu@gmail.com>
2025-12-12 17:01:41 +0900

    2-22③について

---
+++ 2-22/2-22/2-22③/2-22③/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+abstract class Employee
+{
+    public void Work(int hours)
+    {
+    }
+}
+
+class FullTimeEmployee
+{
+    public void Work(int hours)
+    {
+        Console.WriteLine($"{hours}時間働きました。");
+    }
+}
+class InternEmployee
+{
+    public void Work(int hours)
+    {
+        if (hours > 4)
+            Console.WriteLine("最大４時間までしか働けません");
+        else Console.WriteLine($"{hours}時間働きました");
+    }
+}
