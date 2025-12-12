a07cf6964a85ff2d12c0c90c34bbed86d2252b23
Pn0 <superumesyu@gmail.com>
2025-12-12 17:27:49 +0900

    2-22④の課題について

---
+++ 2-22/2-22/2-22④/2-22④/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+interface IWorkable
+{
+    void Work();
+}
+interface IEatable
+{
+    void Eat();
+}
+class Robot : IWorkable
+{
+    public void Work()
+    {
+        Console.WriteLine("ロボットが働きます。");
+    }
+}
+class Engineer : IWorkable,IEatable
+{
+    public void Work()
+    {
+        Console.WriteLine("エンジニアが働きます。");
+    }
+    public void Eat()
+    {
+        Console.WriteLine("エンジニアが食事をします。");
+    }
+}
