9e86e6bbe4e6420cb0a4a0d0d9bc1f1dfe11e35c
Pn0 <superumesyu@gmail.com>
2025-12-12 18:46:27 +0900

    2-22⑤の課題について

---
+++ 2-22/2-22/2-22⑤/2-22⑤/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.CompilerServices;
+
+interface IOutput
+{
+    void write(string text);
+}
+class ConsoleOutput : IOutput
+{
+    public void write(string text)
+    {
+        Console.WriteLine($"出力内容は{text}です。");
+    }
+}
+
+class FileOutput : IOutput
+{
+    public void write( string text )
+    {
+        Console.WriteLine($"出力内容は{text}です。");
+    }
+}
+class ReportPrinter
+{
+    private IOutput OutPut;
+    public ReportPrinter( IOutput output )
+    {
+        OutPut = output;
+    }
+
+    public void Print( string text )
+    {
+        OutPut.write( text );
+    }
+}
