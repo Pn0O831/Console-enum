398707363c4e8b88824e9c9b58438024c67436a3
Pn0 <superumesyu@gmail.com>
2025-12-12 11:46:27 +0900

    2-22①の課題について

---
+++ 2-22/2-22/2-22-①/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+//①
+class Report
+{
+    public string EmployeeName;
+    public int HoursWorked;
+}
+
+class ReportCalculator
+{
+    int CalculateWage(Report report)
+    {
+        int wage = 1250 * report.HoursWorked;
+        return wage;
+    }
+}
+class ReportSaver
+{
+    void Save(Report report,string filePath)
+    {
+        File.WriteAllText(filePath,$"{report.EmployeeName},");
+        Console.WriteLine($"{report.EmployeeName}を、保存しました。");
+    }
+}
