8a93949a14bc2276492b64f5dcf85c24a659e48e
Pn0 <superumesyu@gmail.com>
2025-12-12 14:24:08 +0900

    2-22②について提出

---
+++ 2-22/2-22/2-22②/2-22②/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+interface IBillable
+{
+    int CostForDay(int hoursWorked);
+}
+
+class FullTimeEmployee : IBillable
+{
+    public int CostForDay(int hoursWorked) => 1250 * hoursWorked;
+}
+
+class ContractEmployee : IBillable
+{
+    public int CostForDay(int hoursWorked) => 1000 * hoursWorked;
+}
